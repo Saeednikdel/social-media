76da9456193b9f57bd219c1df5acedbf65a368d6
Saeed Nikdel <1991saeed@gmail.com>
2022-06-04 19:04:16 +0430

    fixed safari bug

--- frontend/src/utils/linkify.js
+++ frontend/src/utils/linkify.js
@@ -1,5 +1,5 @@
 const reUrl = /(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-&?=%.]+/gi;
-const reHash = /(?<!\S)#\S+|\S+#(?!\S)/g;
+const reHash = /(?:^|\s)(#[^\s#]+|[^\s#]+#)(?=$|\s)/g;
 const reAt = /(?:\s|^)?@[A-Za-z0-9\-\.\_]+(?:\s|$)/g;
 const hash_url = `${process.env.REACT_APP_API_URL}/search`;
 const at_url = `${process.env.REACT_APP_API_URL}/profile`;
@@ -15,17 +15,17 @@ export default function (text) {
       return `<a target="_blank" class="tag" href="//${newUrl}">${newUrl}</a>`;
     })
     .replace(
-      reHash,
-      (hash) =>
-        `<a class="tag" href="${hash_url}?keyword=${hash
-          .replace("#", "")
-          .trim()}">${hash}</a>`
-    )
-    .replace(
       reAt,
       (at) =>
         `<a class="tag" href="${at_url}/${at
           .replace("@", "")
           .trim()}">${at}</a>`
+    )
+    .replace(
+      reHash,
+      (hash) =>
+        `<a class="tag" href="${hash_url}?keyword=${hash
+          .replace("#", "")
+          .trim()}">${hash}</a>`
     );
 }
