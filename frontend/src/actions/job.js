7ef79e5c7c107766cfd4793bd45180fe6eded2f3
Saeed Nikdel <1991saeed@gmail.com>
2022-06-11 22:17:49 +0430

    add load request resume

--- frontend/src/actions/job.js
+++ frontend/src/actions/job.js
@@ -12,7 +12,65 @@ import {
   BOOKMARK_JOB_FAIL,
   LOAD_BOOKMARK_JOB_SUCCESS,
   LOAD_BOOKMARK_JOB_FAIL,
+  LOAD_REQUEST_JOB_SUCCESS,
+  LOAD_REQUEST_JOB_FAIL,
+  LOAD_REQUESTED_RESUME_SUCCESS,
+  LOAD_REQUESTED_RESUME_FAIL,
 } from "./types";
+
+export const load_requested_resume =
+  (id, requested_user) => async (dispatch) => {
+    if (localStorage.getItem("access")) {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `JWT ${localStorage.getItem("access")}`,
+          Accept: "application/json",
+        },
+      };
+      const user = localStorage.getItem("id");
+      const body = JSON.stringify({ user, id, requested_user });
+      try {
+        const res = await axios.post(
+          `${process.env.REACT_APP_API_URL}/api/jobs/see-resume/`,
+          body,
+          config
+        );
+        dispatch({
+          type: LOAD_REQUESTED_RESUME_SUCCESS,
+          payload: res.data,
+        });
+      } catch (err) {
+        dispatch({
+          type: LOAD_REQUESTED_RESUME_FAIL,
+        });
+      }
+    }
+  };
+export const load_job_requests = (id, page) => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  };
+  try {
+    const res = await axios.get(
+      `${process.env.REACT_APP_API_URL}/api/jobs/job-request-list/${id}/${page}/`,
+      config
+    );
+    dispatch({
+      type: LOAD_REQUEST_JOB_SUCCESS,
+      payload: res.data,
+      page: page,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOAD_REQUEST_JOB_FAIL,
+    });
+  }
+};
+
 export const load_jobs = (page, keyword) => async (dispatch) => {
   const config = {
     headers: {
@@ -44,7 +102,7 @@ export const load_jobs = (page, keyword) => async (dispatch) => {
     });
   }
 };
-export const load_user_posts = (name, page) => async (dispatch) => {
+export const load_user_jobs = (name, page) => async (dispatch) => {
   const config = {
     headers: {
       "Content-Type": "application/json",
