2aed93a2a0124eaa3e2437de520f56162539e090
Saeed Nikdel <1991saeed@gmail.com>
2022-06-12 19:15:41 +0430

    translate 404

--- frontend/src/components/PageNotFound.js
+++ frontend/src/components/PageNotFound.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
+import translate from "../translate";
+
 function PageNotFound() {
   return (
     <div style={{ textAlign: "center", marginTop: 120, marginBottom: 120 }}>
-      <Typography variant="h6">صفحه مورد نظر یافت نشد. 404!</Typography>
+      <Typography variant="h6">{translate("Page not found. 404")}</Typography>
     </div>
   );
 }
