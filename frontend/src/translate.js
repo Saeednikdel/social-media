119a40371a2b43ffdd9432bdc611c82a77d5be43
SaeedNikdell <1991saeed@gmail.com>
2022-06-17 17:44:08 +0430

    add translation for multiple langs

--- frontend/src/translate.js
+++ frontend/src/translate.js
@@ -1,16 +1,27 @@
 import React from "react";
 import fa from "./assets/fa";
+import so from "./assets/so";
+import ku from "./assets/ku";
+import tr from "./assets/tr";
+import ar from "./assets/ar";
 export default function translate(text) {
   let lang = localStorage.getItem("lang") ? localStorage.getItem("lang") : "en";
-  if (localStorage.getItem("lang") == null) {
-    localStorage.setItem("lang", "en");
-  }
+  localStorage.getItem("lang") == null && localStorage.setItem("lang", "en");
+
   switch (lang) {
-    case "en":
-      return text;
     case "fa":
       return fa[text];
+    case "so":
+      return so[text];
+    case "ku":
+      return ku[text];
+    case "tr":
+      return tr[text];
+    case "ar":
+      return ar[text];
+    case "en":
+      return text;
     default:
-      break;
+      return text;
   }
 }
